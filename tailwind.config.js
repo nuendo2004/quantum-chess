03de565991dbdbb6ea135da2f31278f7e4b2714c
apatil45 <apatil45@stevens.edu>
2025-04-17 23:01:19 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+      gate: '#00BFFF',     // bright cyan
+      wall: '#2c2c4a',
+      door: '#6B4226',
+      glowYellow: '#FFD700',
+      exitGlow: '#FF69B4',
+    },
+  },
+  },
+  plugins: [],
+}
+
+
